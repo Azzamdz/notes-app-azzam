1c217e0b4f3a9abfbdbf0ddf2419275cf7e70ea6
Azzamdz <azzamdzikri23.12.08@gmail.com>
2025-09-24 17:45:26 +0700

    menambahkan add and getbyid

---
+++ backend/src/routes/notesRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { getAllNotesHandler } from "../handlers/notesHandler.js";
+import { addNoteHandler } from "../handlers/notesHandler.js";
+import { getNoteByIdHandler } from "../handlers/notesHandler.js";
+
+const noteRouter = express.Router();
+
+noteRouter.get("/notes", getAllNotesHandler);
+noteRouter.post("/notes", addNoteHandler);
+noteRouter.get("/notes/:id", getNoteByIdHandler);
+
+export default noteRouter;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { testConnection } from "./config/db.js";
 import helloRouter from "./routes/helloRoute.js";
+import noteRouter from "./routes/notesRoute.js";
 
 const app = express();
+app.use(express.json());
 const port = 3000;
 
-app.use("/", helloRouter);
+app.use(helloRouter);
+app.use(noteRouter);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
   testConnection();
